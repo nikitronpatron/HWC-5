39f804ffac1c3161b28f5538dc9345b26f27ccd5
nikitronpatron <nikita.borsh9876@gmail.com>
2022-11-15 20:56:58 +0300

    EX34

---
+++ EX34/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] myArray = CreateRandomArray(10, 100, 999);
+ShowArray(myArray);
+
+int count = 0;
+for(int i = 0; i < myArray.Length; i++)
+{
+    if(myArray[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
