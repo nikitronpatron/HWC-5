280785d1bb4d346fdaf29fd00e40debf263c1480
nikitronpatron <nikita.borsh9876@gmail.com>
2022-11-15 20:57:22 +0300

    EX36

---
+++ EX36/Program.cs
@@ -0,0 +1,46 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] myArray = CreateRandomArray(10, 100, 999);
+ShowArray(myArray);
+
+//Для нечетных по индексу
+int sumArrayIndex = 0;
+for(int i = 0; i < myArray.Length; i++)
+{
+    if(i % 2 != 0)
+    {
+        sumArrayIndex += myArray[i];
+    }
+}
+Console.WriteLine(sumArrayIndex);
+
+//Для нечетных по визуальной составляющей
+int sumArrayVisual = 0;
+for(int i = 0; i < myArray.Length; i++)
+{
+    if(i % 2 == 0)
+    {
+        sumArrayVisual += myArray[i];
+    }
+}
+Console.WriteLine(sumArrayVisual);
